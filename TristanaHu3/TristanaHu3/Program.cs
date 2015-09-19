5bb375a49067dfa714d7194b646354e721f8ccac
Mario Gabriell <mario_gk@hotmail.com>
2015-09-19 16:57:20 -0300

    Update

--- TristanaHu3/TristanaHu3/Program.cs
+++ TristanaHu3/TristanaHu3/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using EloBuddy;
 using EloBuddy.SDK;
 using EloBuddy.SDK.Enumerations;

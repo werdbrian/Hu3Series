c8a8789ba47abb246c890a1f8c5155ceb5edbe0f
Mario Gabriell <mario_gk@hotmail.com>
2015-09-19 23:59:05 -0300

    Update

--- KatarinaHu3/KatarinaHu3/Program.cs
+++ KatarinaHu3/KatarinaHu3/Program.cs
@@ -46,7 +46,7 @@ namespace KatarinaHu3
             R = new Spell.Active(SpellSlot.R, 540);
 
             KatarinaMenu = MainMenu.AddMenu("KatarinaHu3", "katarinahu3");
-            KatarinaMenu.AddGroupLabel("Katarina Hu3 1.3");
+            KatarinaMenu.AddGroupLabel("Katarina Hu3 1.4");
             KatarinaMenu.AddSeparator();
             KatarinaMenu.AddLabel("Made By MarioGK");
             SettingsMenu = KatarinaMenu.AddSubMenu("Settings", "Settings");

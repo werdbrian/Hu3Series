b31ec95890fbb0acecceb83f133df365133ee704
Mario Gabriell <mario_gk@hotmail.com>
2015-09-27 13:52:56 -0300

    Release Master Yi

---
+++ MasterYiHu3/MasterYiHu3/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Linq;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+using EloBuddy.SDK.Utils;
+using Color = System.Drawing.Color;
+using SharpDX;
+
+namespace MasterYiHu3
+{
+    class Program
+    {
+        public static Spell.Targeted Q;
+        public static Spell.Active W;
+        public static Spell.Active E;
+        public static Spell.Active R;
+        public static Menu Menu, SettingsMenu;
+
+
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+            Bootstrap.Init(null);
+        }
+
+        public static AIHeroClient _Player
+        {
+            get { return ObjectManager.Player; }
+        }
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            if (Player.Instance.ChampionName != "MasterYi")
+                return;
+
+            Q = new Spell.Targeted(SpellSlot.Q, 600);
+            W = new Spell.Active(SpellSlot.W);
+            E = new Spell.Active(SpellSlot.E);
+            R = new Spell.Active(SpellSlot.R);
+
+            Menu = MainMenu.AddMenu("MasterYiHu3", "masteryihu3");
+            Menu.AddGroupLabel("MasterYi Hu3 Test Version 0.1");
+            Menu.AddSeparator();
+            Menu.AddLabel("Made By MarioGK");
+            SettingsMenu = Menu.AddSubMenu("Settings", "Settings");
+            SettingsMenu.AddGroupLabel("Settings");
+            SettingsMenu.AddLabel("Combo");
+            SettingsMenu.Add("Qc", new CheckBox("Use Q on Combo"));
+            SettingsMenu.Add("Ec", new CheckBox("Use E on Combo"));
+            SettingsMenu.Add("Rc", new CheckBox("Use R on Combo"));
+            SettingsMenu.AddLabel("Harass");
+            SettingsMenu.Add("Qh", new CheckBox("Use Q on Harass"));
+            SettingsMenu.AddLabel("LaneClear");
+            SettingsMenu.Add("Qlc", new CheckBox("Use Q on LaneClear"));
+            SettingsMenu.AddLabel("LastHit");
+            SettingsMenu.Add("Qlh", new CheckBox("Use Q on LastHit"));
+            SettingsMenu.AddLabel("KillSteal");
+            SettingsMenu.Add("Qkill", new CheckBox("Use Q KillSteal"));
+            SettingsMenu.AddLabel("Draw");
+            SettingsMenu.Add("Qdraw", new CheckBox("Draw Q"));
+
+            Game.OnTick += Game_OnTick;
+            Drawing.OnDraw += Drawing_OnDraw;
+        }
+        private static void Game_OnTick(EventArgs args)
+        {
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
+            {
+                Combo();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass))
+            {
+                Harass();
+            }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear))
+            {
+                LaneClear();
+            }
+
+            KillSteal();
+        }
+        public static float GetDamage(SpellSlot spell, Obj_AI_Base target)
+        {
+            float ap = _Player.FlatMagicDamageMod + _Player.BaseAbilityDamage;
+            float ad = _Player.FlatMagicDamageMod + _Player.BaseAttackDamage;
+            if (spell == SpellSlot.Q)
+            {
+                if (!Q.IsReady())
+                    return 0;
+                return _Player.CalculateDamageOnUnit(target, DamageType.Magical, 25f + 35f * (Q.Level - 1) + 100 / 100 * ad);
+            }
+            return 0;
+        }
+
+        private static void KillSteal()
+        {
+        }
+
+        private static void Combo()
+        {
+            var target = TargetSelector.GetTarget(_Player.AttackRange, DamageType.Physical);
+            var useQ = SettingsMenu["Qc"].Cast<CheckBox>().CurrentValue;
+            var useE = SettingsMenu["Ec"].Cast<CheckBox>().CurrentValue;
+            var useR = SettingsMenu["Rc"].Cast<CheckBox>().CurrentValue;
+
+            if (useQ && Q.IsReady() && target.IsValidTarget(Q.Range))
+            {
+                Q.Cast(target);
+            }
+            if (useE && E.IsReady() && target.IsValidTarget(150))
+            {
+                E.Cast();
+            }
+            if (useR && R.IsReady() && target.IsValidTarget(200))
+            {
+                R.Cast();
+            }
+        }
+
+        private static void Harass()
+        {
+            var target = TargetSelector.GetTarget(_Player.AttackRange, DamageType.Physical);
+            var useQ = SettingsMenu["Qc"].Cast<CheckBox>().CurrentValue;
+            if (useQ && Q.IsReady() && target.IsValidTarget(Q.Range))
+            {
+                Q.Cast(target);
+            }
+        }
+
+        private static void LaneClear()
+        {
+            var minion = ObjectManager.Get<Obj_AI_Minion>().FirstOrDefault(a => a.IsEnemy && !a.IsDead && a.Distance(_Player) < Q.Range);
+            var useQ = SettingsMenu["Qlc"].Cast<CheckBox>().CurrentValue;
+            if (useQ && Q.IsReady() && minion.Health <= GetDamage(SpellSlot.Q, minion))
+            {
+                Q.Cast(minion);
+            }
+        }
+
+        private static void LastHit()
+        {
+            var minion = ObjectManager.Get<Obj_AI_Minion>().FirstOrDefault(a => a.IsEnemy && !a.IsDead && a.Distance(_Player) < Q.Range);
+            var useQ = SettingsMenu["Qlh"].Cast<CheckBox>().CurrentValue;
+            if (useQ && Q.IsReady() && minion.Health <= GetDamage(SpellSlot.Q, minion))
+            {
+                Q.Cast(minion);
+            }
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            if (SettingsMenu["drawQ"].Cast<CheckBox>().CurrentValue)
+            {
+                new Circle() { Color = Color.Green, BorderWidth = 1, Radius = Q.Range }.Draw(_Player.Position);
+            }
+        }
+    }
+}
